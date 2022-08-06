b224b6ba2d6eb986ee2d207af839d9f27758e7f8
DanielC <danielcordero1998@gmail.com>
2022-08-06 09:11:11 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+
+<div class="main container">
+    <div class="main-title">
+        <h1>Bubble Breaker</h1>
+    </div>
+    <div>
+        <a href="game/nuevo_juego.php">Nuevo juego</a>
+    </div>
+</div>
